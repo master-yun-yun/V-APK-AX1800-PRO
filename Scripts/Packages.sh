6b09befec3c93bfbd7dc427d1e8e16a2338864ec
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2026-05-26 22:29:58 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -193,8 +193,7 @@ UPDATE_PACKAGE "luci-app-openclaw" "10000ge10000/luci-app-openclaw" "main"
 #-------------------2026.04.06以上小龙虾----------------------------#
 
 #-----------2026.05.24-----------#
-# luci-app-taskplan 任务设置2.0版
-UPDATE_PACKAGE "luci-app-taskplan" "sirpdboy/luci-app-taskplan" "main"
+
 # 应用过滤-----kiddin9/op-packages
 #UPDATE_PACKAGE "open-app-filter" "kiddin9/op-packages" "main" "pkg"
 #UPDATE_PACKAGE "oaf" "kiddin9/op-packages" "main" "pkg"
